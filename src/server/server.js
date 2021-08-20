5e4c23060aff36a38c4067852b1465cf146eec5c
Dominic Law <dominic.law79@gmail.com>
2021-08-20 14:48:53 +1000

    Add comemnting + POST calls

--- src/server/server.js
+++ src/server/server.js
@@ -16,13 +16,47 @@ let poems = [
   }
 ];
 
+// @desc default page
+// @route GET /
 app.get("/", (req, res) => {
   res.send("<h1>PoemShare Server</h1>");
 });
 
+// @desc fetch poems data
+// @route GET /api/poems
 app.get("/api/poems", (req, res) => {
+  console.log(poems);
   res.json(poems);
 });
+
+// @desc fetch specific poem
+// @route GET /api/poems:id
+app.get("/api/poems/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const poem = poems.find(poem => poem.id == id);
+    if (poem) {
+        res.json(poem);
+    } else {
+        res.status(404).end();
+    }
+});
+
+// @desc post poem
+// @route POST /api/poems
+app.post("/api/poems", (req, res) => {
+    const id = poems.length;
+    var postBody = req.body;
+    var poem = {
+        id: id,
+        title: postBody.title,
+        author: postBody.author,
+        authorid: postBody.authorid,
+        text: postBody.text,
+        votes: 0
+    }
+    poems = poems.concat(poem)
+    res.json(poem)
+})
 
 const PORT = 3001;
 app.listen(PORT, () => {
