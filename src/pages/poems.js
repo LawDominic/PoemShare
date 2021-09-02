ae38cd91580450c1a001e6268cac77999ea00942
Dominic Law <dominic.law79@gmail.com>
2021-09-02 18:36:09 +1000

    Added markdown CSS

--- src/pages/poems.js
+++ src/pages/poems.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useParams} from 'react-router-dom'
 import ReactMarkdown from 'react-markdown'
+import style from './markdownCSS.module.css';
 
 const Poem = ({poems}) => {
     const id = Number(useParams().id)
@@ -10,7 +11,7 @@ const Poem = ({poems}) => {
         <div className="space-y-4 text-gray-900 w-2/3 mx-auto">
             <p className="text-2xl font-semibold">{poem.title}</p>
             <p>by <i>{poem.author}</i></p><br/>
-            <ReactMarkdown children={poem.text}/>
+            <ReactMarkdown children={poem.text} className={style.reactMarkDown}/>
         </div>
     )
 }
