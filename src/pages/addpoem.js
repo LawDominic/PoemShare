3a3174b3de5807f02c7844bbe8a167fb8bf5018e
Dominic Law <dominic.law79@gmail.com>
2021-09-04 00:10:13 +1000

    Added invalid alert

--- src/pages/addpoem.js
+++ src/pages/addpoem.js
@@ -1,19 +1,21 @@
-import React, { useState } from 'react';
-import {Link} from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import {Link, useHistory} from 'react-router-dom'
 import ReactMarkdown from 'react-markdown'
 import style from './markdownCSS.module.css';
-
-const AddPoem = ({addPoem, poems}) => {
+import poemServices from '../services/poems.js';
+const AddPoem = ({poems}) => {
+  const [showInvalidAlert, setShowInvalidAlert] = useState(false);
   const [showFailedAlert, setShowFailedAlert] = useState(false);
   const [showSuccessAlert, setShowSuccessAlert] = useState(false);
-  const FailedAlert = () => {
+
+  const InvalidAlert = () => {
       return (
-        showFailedAlert ? (
+        showInvalidAlert ? (
           <div className={"text-white px-6 py-4 border-0 rounded relative mb-4 bg-red-400"}>
             <p className="inline-block align-middle mr-3">
               <b>Failed</b> - make sure none of the fields are empty!
             </p>
-            <button className="bg-transparent right-0 top-0 mt-4 mr-6 text-2xl font-bold leading-none absolute focus:outline-none" onClick={() => setShowFailedAlert(false)}>
+            <button className="bg-transparent right-0 top-0 mt-4 mr-6 text-2xl font-bold leading-none absolute focus:outline-none" onClick={() => setShowInvalidAlert(false)}>
               <p>×</p>
             </button>
           </div>
@@ -21,13 +23,28 @@ const AddPoem = ({addPoem, poems}) => {
     )
   }
 
+  const FailedAlert = () => {
+    return (
+      showFailedAlert ? (
+        <div className={"text-white px-6 py-4 border-0 rounded relative mb-4 bg-red-400"}>
+          <p className="inline-block align-middle mr-3">
+            <b>Failed</b> - make sure none of the fields are empty!
+          </p>
+          <button className="bg-transparent right-0 top-0 mt-4 mr-6 text-2xl font-bold leading-none absolute focus:outline-none" onClick={() => setShowFailedAlert(false)}>
+            <p>×</p>
+          </button>
+        </div>
+      ) : null
+  )
+}
+
   const SuccessAlert = () => {
-    const id = poems.length-1
+    const id = poems.length
     return (
       showSuccessAlert ? (
         <div className={"text-white px-6 py-4 border-0 rounded relative mb-4 bg-green-400"}>
           <p className="inline-block align-middle mr-3">
-            <b>Success</b> - head back to the <Link to={'/'} className="font-base font-semibold">home page</Link> to view your poem or <Link to={`/poems/${id}`} className="font-base font-semibold">directly here</Link>!
+            <b>Success</b> - head back to the <Link to={'/'} className="font-base font-semibold">home page</Link> to view your poem or <Link to={`/poems/${id}`} className="font-base font-semibold">directly here</Link>
           </p>
           <button className="bg-transparent right-0 top-0 mt-4 mr-6 text-2xl font-bold leading-none absolute focus:outline-none" onClick={() => setShowSuccessAlert(false)}>
             <p>×</p>
@@ -44,20 +61,25 @@ const AddPoem = ({addPoem, poems}) => {
   const formHandler = (event) => {
     event.preventDefault()
     if (newPoemTitle.length === 0 || newPoemAuthor.length === 0 || newPoemText.length === 0) {
-      setShowFailedAlert(true)
+      setShowInvalidAlert(true)
     } else {
       const poemObject = {
         title: newPoemTitle,
         author: newPoemAuthor,
         text: newPoemText
       }
-      addPoem(poemObject)
-      setShowSuccessAlert(true)
-      setNewPoemTitle('')
-      setNewPoemAuthor('')
-      setNewPoemText('')
+      poemServices.create(poemObject)
+      .then((obj) => {
+        if (obj.id && obj.author && obj.text) {
+          setNewPoemTitle('')
+          setNewPoemAuthor('')
+          setNewPoemText('')
+          setShowSuccessAlert(true)
+        } else {
+          setShowFailedAlert(true)
+        }
+      })  
     }
-    
   }
 
   const handleNewPoemTitle = (event) => {
@@ -74,13 +96,14 @@ const AddPoem = ({addPoem, poems}) => {
 
     return (
       <div>
+          <InvalidAlert/>
           <FailedAlert/>
           <SuccessAlert/>
           <form className="space-y-4 text-gray-900 w-2/3 mx-auto" onSubmit={formHandler}>
           <div className="flex flex-wrap -mx-2 space-y-4 md:space-y-0">
             <div className="w-full px-2 md:w-1/2">
               <label className="block mb-1" htmlFor="title">Title</label>
-              <input autofocus className="w-full h-10 px-3 text-base placeholder-gray-600 border rounded-lg focus:outline-none" type="text" id="title" value={newPoemTitle} onChange={handleNewPoemTitle}/>
+              <input autoFocus className="w-full h-10 px-3 text-base placeholder-gray-600 border rounded-lg focus:outline-none" type="text" id="title" value={newPoemTitle} onChange={handleNewPoemTitle}/>
             </div>
             <div className="w-full px-2 md:w-1/2">
               <label className="block mb-1" htmlFor="author">Author</label>
